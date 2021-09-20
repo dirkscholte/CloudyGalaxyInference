169e87ba07372df0a5aabd8c5b9022f50e2c9ebf
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-09-20 14:43:34 +0100

    Train model for 'OII_3726', 'OII_3729', 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584'

--- CloudyGalaxyInference/train_sbi_network.py
+++ CloudyGalaxyInference/train_sbi_network.py
@@ -12,7 +12,7 @@ from gaussian_noise_model import GaussianNoiseModel
 from sbi import utils as utils
 from sbi.inference.base import infer
 
-# Train model for 'OII_3726', 'OII_3729', 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584', 'SII_6716', 'SII_6731'
+# Train model for 'OII_3726', 'OII_3729', 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584'
 
 #import photoionization models
 path = '/Users/dirk/Documents/PhD/scripts/CloudyGalaxy/models/test_model_high_res/'
@@ -22,12 +22,12 @@ model_parameters = np.load(path + 'test_model_high_res_age_2Myr_n_tau_-0p7_param
 model_derived_parameters = np.load(path + 'test_model_high_res_age_2Myr_n_tau_-0p7_derived_parameters_file.npy')
 
 interpolated_grid = InterpolateModelGrid(model_labels, model_flux, model_parameters, model_derived_parameters, normalize_by='H__1_656281A')
-interpolated_flux = interpolated_grid.interpolate_flux(['O__2_372603A', 'O__2_372881A', 'H__1_486133A', 'O__3_495891A', 'O__3_500684A', 'N__2_654800A', 'H__1_656281A', 'N__2_658345A', 'S__2_671644A', 'S__2_673082A'])
+interpolated_flux = interpolated_grid.interpolate_flux(['O__2_372603A', 'O__2_372881A', 'H__1_486133A', 'O__3_495891A', 'O__3_500684A', 'N__2_654800A', 'H__1_656281A', 'N__2_658345A'])
 
-line_labels = ['OII_3726', 'OII_3729', 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584', 'SII_6716', 'SII_6731']
+line_labels = ['OII_3726', 'OII_3729', 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584']
 line_flux_labels = [label+'_FLUX' for label in line_labels]
 line_flux_ivar_labels = [label+'_FLUX_IVAR' for label in line_labels]
-line_wavelengths = [3727., 3729., 4862., 4960., 5008., 6549., 6564., 6585., 6718., 6732.]
+line_wavelengths = [3727., 3729., 4862., 4960., 5008., 6549., 6564., 6585.]
 
 denali_fastspec = Table.read("/Users/dirk/Documents/PhD/scripts/desi/data/Denali/fastspec-denali-cumulative.fits", hdu=1)
 names = [name for name in denali_fastspec.colnames if len(denali_fastspec[name].shape) <= 1]
@@ -88,4 +88,4 @@ prior = utils.BoxUniform(low = torch.tensor([10, -1., -4., 0.1, -2.]),
 print(prior)
 posterior = infer(simulation, prior, 'SNPE', num_simulations=10000)
 
-torch.save(posterior.net, 'sbi_inference_model_DESI_BGS_OII_OII_Hb_OIII_OIII_NII_Ha_NII_SII_SII_v11')
+torch.save(posterior.net, 'sbi_inference_model_DESI_BGS_OII_OII_Hb_OIII_OIII_NII_Ha_NII_v1')
