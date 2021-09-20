77a641fe018ddec0dd32adb280688cd58a337cf9
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-09-20 14:38:03 +0100

    Add extinction correction to the data before noise model is made.

--- CloudyGalaxyInference/train_sbi_network.py
+++ CloudyGalaxyInference/train_sbi_network.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 from astropy.table import Table
+import astropy.units as u
 import torch
-import corner
 
-import matplotlib.pyplot as plt
+from galactic_extinction_correction import galactic_extinction_correction, rest_to_obs_wavelength
 
 from interpolate_model_grid import InterpolateModelGrid
 from gaussian_noise_model import GaussianNoiseModel
@@ -14,12 +14,12 @@ from sbi.inference.base import infer
 
 #import photoionization models
 path = '/Users/dirk/Documents/PhD/scripts/CloudyGalaxy/models/test_model_high_res/'
-model_labels = list(np.load(path + 'test_model_high_res_age_2Myr_logtau_emission_line_labels.npy'))
-model_flux = np.load(path + 'test_model_high_res_age_2Myr_logtau_emission_line_luminosity_file.npy')
-model_parameters = np.load(path + 'test_model_high_res_age_2Myr_logtau_parameters_file.npy')
-model_derived_parameters = np.load(path + 'test_model_high_res_age_2Myr_logtau_derived_parameters_file.npy')
+model_labels = list(np.load(path + 'test_model_high_res_age_2Myr_n_tau_-0p7_emission_line_labels.npy'))
+model_flux = np.load(path + 'test_model_high_res_age_2Myr_n_tau_-0p7_emission_line_luminosity_file.npy')
+model_parameters = np.load(path + 'test_model_high_res_age_2Myr_n_tau_-0p7_parameters_file.npy')
+model_derived_parameters = np.load(path + 'test_model_high_res_age_2Myr_n_tau_-0p7_derived_parameters_file.npy')
 
-interpolated_grid = InterpolateModelGrid(model_labels, model_flux, model_parameters, normalize_by='H__1_656281A')
+interpolated_grid = InterpolateModelGrid(model_labels, model_flux, model_parameters, model_derived_parameters, normalize_by='H__1_656281A')
 interpolated_flux = interpolated_grid.interpolate_flux(['O__2_372603A', 'O__2_372881A', 'H__1_486133A', 'O__3_495891A', 'O__3_500684A', 'N__2_654800A', 'H__1_656281A', 'N__2_658345A', 'S__2_671644A', 'S__2_673082A'])
 
 line_labels = ['OII_3726', 'OII_3729', 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584', 'SII_6716', 'SII_6731']
@@ -36,11 +36,25 @@ denali_fastspec_hdu2 = Table.read("/Users/dirk/Documents/PhD/scripts/desi/data/D
 gal_mask = denali_fastspec_hdu2['SPECTYPE']==b'GALAXY'
 sn_mask = denali_fastspec['HALPHA_FLUX']*(denali_fastspec['HALPHA_FLUX_IVAR']**0.5) > 25.0
 z_mask = denali_fastspec['CONTINUUM_Z']<0.5
-sf_mask = [np.log10(denali_fastspec["OIII_5007_FLUX"]/denali_fastspec["HBETA_FLUX"]) < 0.61*(np.log10(denali_fastspec["NII_6584_FLUX"]/denali_fastspec["HALPHA_FLUX"]) + 0.05)**-1 + 1.3][0]
+sf_mask = ~[np.log10(denali_fastspec["OIII_5007_FLUX"]/denali_fastspec["HBETA_FLUX"]) > 0.61*(np.log10(denali_fastspec["NII_6584_FLUX"]/denali_fastspec["HALPHA_FLUX"]) + 0.05)**-1 + 1.3][0]
 line_num_mask = np.sum(denali_fastspec[line_flux_labels].to_numpy()!=0.0, axis=1)>=4.
 
 denali_fastspec_hdu2 = denali_fastspec_hdu2[gal_mask & sn_mask & z_mask & sf_mask].reset_index()
 denali_fastspec = denali_fastspec[gal_mask & sn_mask & z_mask & sf_mask].reset_index()
+
+#Extinction correction
+extinction_correction_factor = np.ones((len(denali_fastspec), 10))
+
+for i in range(len(denali_fastspec)):
+    print('Extinction correction: ', i)
+    obs_wavelength = rest_to_obs_wavelength(line_wavelengths * u.angstrom, denali_fastspec['CONTINUUM_Z'][i])
+    extinction_correction_factor[i] = galactic_extinction_correction(denali_fastspec_hdu2['RA'][i]*u.degree, denali_fastspec_hdu2['DEC'][i]*u.degree, obs_wavelength, np.ones_like(line_wavelengths)*u.erg * u.cm**-2 * u.s**-1).value
+
+for i in range(len(line_labels)):
+    print(extinction_correction_factor[i])
+    denali_fastspec[line_labels[i]+'_FLUX'] = denali_fastspec[line_labels[i]+'_FLUX'] * extinction_correction_factor[:, i]
+    denali_fastspec[line_labels[i]+'_FLUX_ERR'] = denali_fastspec[line_labels[i]+'_FLUX_IVAR']**-0.5 * extinction_correction_factor[:, i]
+
 
 flux_catalogue = pd.DataFrame(denali_fastspec[line_flux_labels].to_numpy(), columns=line_labels)
 sn_catalogue = pd.DataFrame(denali_fastspec[line_flux_labels].to_numpy() * denali_fastspec[line_flux_ivar_labels].to_numpy()**0.5, columns=line_labels)
@@ -67,33 +81,9 @@ def simulation(theta, redshift='random'):
 
 num_dim = 5
 prior = utils.BoxUniform(low = torch.tensor([10, -1., -4., 0.1, -2.]),
-                         high= torch.tensor([400, 0.7, 0.5, 0.6, 0.6]))
+                         high= torch.tensor([400, 0.7, -1., 0.6, 0.6]))
 
 print(prior)
-posterior = infer(simulation, prior, 'SNPE', num_simulations=100000)
+posterior = infer(simulation, prior, 'SNPE', num_simulations=10000)
 
-torch.save(posterior.net, 'sbi_inference_model_DESI_BGS_OII_OII_Hb_OIII_OIII_NII_Ha_NII_SII_SII')
-
-posterior = infer(simulation, prior, 'SNPE', num_simulations=10)
-posterior.net = torch.load('../models/sbi_inference_model_DESI_BGS_OII_OII_Hb_OIII_OIII_NII_Ha_NII_SII_SII')
-x_o_1s = []
-for i in range(1):
-    torch.manual_seed(i+11)
-    theta = torch.tensor([50, 0.0,-1.43,0.3,0.0])
-    x_o_1 = simulation(theta, redshift=0.48)
-    x_o_1s.append(x_o_1)
-
-print('start')
-for i in range(10):
-    x_o_1 = denali_fastspec[line_flux_labels+line_flux_ivar_labels].to_numpy()[30+i]
-
-    print(x_o_1)
-    posterior_samples_1 = posterior.sample((10000,), x=x_o_1)
-    posterior_samples_1 = posterior_samples_1.numpy()
-    print(posterior_samples_1.shape)
-    gas = posterior_samples_1[:,4] - np.log10(posterior_samples_1[:,3]) - posterior_samples_1[:,1] - np.log10(0.0142)
-    print(gas.shape)
-    posterior_samples_1 = np.hstack([posterior_samples_1,np.expand_dims(gas, axis=-1)])
-
-    corner.corner(posterior_samples_1, show_titles=True, quantiles=[0.16, 0.5, 0.84], labels=['Amplitude', 'Z', 'U', 'xi', 'tau', 'gas'])
-    plt.show()
+torch.save(posterior.net, 'sbi_inference_model_DESI_BGS_OII_OII_Hb_OIII_OIII_NII_Ha_NII_SII_SII_v11')
