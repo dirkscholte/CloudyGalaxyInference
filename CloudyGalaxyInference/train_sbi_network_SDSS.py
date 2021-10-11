7ed65046cee8c95159f2be29d6120f45d2227b01
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-10-11 11:50:18 +0100

    Update SDSS training to only include Hb OIII OIII NII Ha NII SII SII lines.

--- CloudyGalaxyInference/train_sbi_network_SDSS.py
+++ CloudyGalaxyInference/train_sbi_network_SDSS.py
@@ -10,7 +10,7 @@ from sbi import utils as utils
 from sbi.inference import SNPE, simulate_for_sbi
 
 
-# Train model for 'OII_3726', 'OII_3729', 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584', 'SII_6716', 'SII_6731'
+# Train model for 'HBETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'HALPHA', 'NII_6584', 'SII_6716', 'SII_6731'
 
 #import photoionization models
 path = '/Users/dirk/Documents/PhD/scripts/CloudyGalaxy/models/test_model_high_res/'
@@ -20,12 +20,12 @@ model_parameters = np.load(path + 'full_model_high_res_age_2Myr_unattenuated_par
 model_derived_parameters = np.load(path + 'full_model_high_res_age_2Myr_unattenuated_derived_parameters_file.npy')
 
 interpolated_grid = InterpolateModelGrid(model_labels, model_flux, model_parameters, model_derived_parameters, normalize_by='H__1_656281A')
-interpolated_flux = interpolated_grid.interpolate_flux(['O__2_372603A', 'O__2_372881A', 'H__1_486133A', 'O__3_495891A', 'O__3_500684A', 'N__2_654800A', 'H__1_656281A', 'N__2_658345A', 'S__2_671644A', 'S__2_673082A'])
+interpolated_flux = interpolated_grid.interpolate_flux(['H__1_486133A', 'O__3_495891A', 'O__3_500684A', 'N__2_654800A', 'H__1_656281A', 'N__2_658345A', 'S__2_671644A', 'S__2_673082A'])
 
-line_labels = ['OII_3726', 'OII_3729', 'H_BETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'H_ALPHA', 'NII_6584', 'SII_6717', 'SII_6731']
+line_labels = ['H_BETA', 'OIII_4959', 'OIII_5007', 'NII_6548', 'H_ALPHA', 'NII_6584', 'SII_6717', 'SII_6731']
 line_flux_labels = [label+'_FLUX' for label in line_labels]
 line_flux_err_labels = [label+'_FLUX_ERR' for label in line_labels]
-line_wavelengths = [3727., 3729., 4862., 4960., 5008., 6549., 6564., 6585., 6718., 6732.]
+line_wavelengths = [4862., 4960., 5008., 6549., 6564., 6585., 6718., 6732.]
 
 sdss_spec_info = Table.read("/Users/dirk/Documents/PhD/scripts/catalogs/galSpecInfo-dr8.fits")
 names = [name for name in sdss_spec_info.colnames if len(sdss_spec_info[name].shape) <= 1]
@@ -41,7 +41,7 @@ print(sdss_spec_info['TARGETTYPE'])
 
 
 # masking operations
-gal_mask = sdss_spec_info['TARGETTYPE']=='GALAXY'
+gal_mask = sdss_spec_info['TARGETTYPE']==b'GALAXY             '
 sn_mask = sdss_spec_line['H_ALPHA_FLUX']/sdss_spec_line['H_ALPHA_FLUX_ERR'] > 25.0
 z_mask = sdss_spec_info['Z']<0.5
 sf_mask = ~[np.log10(sdss_spec_line["OIII_5007_FLUX"]/sdss_spec_line["H_BETA_FLUX"]) > 0.61*(np.log10(sdss_spec_line["NII_6584_FLUX"]/sdss_spec_line["H_ALPHA_FLUX"]) + 0.05)**-1 + 1.3][0]
@@ -49,6 +49,7 @@ sf_mask = ~[np.log10(sdss_spec_line["OIII_5007_FLUX"]/sdss_spec_line["H_BETA_FLU
 flux_catalogue = pd.DataFrame(sdss_spec_line[gal_mask & sn_mask & z_mask & sf_mask][line_flux_labels].to_numpy(), columns=line_labels)
 sn_catalogue = pd.DataFrame(sdss_spec_line[gal_mask & sn_mask & z_mask & sf_mask][line_flux_labels].to_numpy() / sdss_spec_line[gal_mask & sn_mask & z_mask & sf_mask][line_flux_err_labels].to_numpy(), columns=line_labels)
 
+print(flux_catalogue.shape)
 gaussian_noise_model = GaussianNoiseModel(flux_catalogue, sn_catalogue, line_labels, 'H_ALPHA')
 
 def transmission_function(lambda_, logtau, n=-1.3):
@@ -74,7 +75,7 @@ def simulation(theta, redshift='random'):
     sn_level = sn_level
     if redshift=='random':
         redshift = np.random.uniform(low=0.0, high=0.5)
-    rest_wavelength = np.array([3727.,3729.,4861.,4959.,5007.,6548.,6563.,6584.,6717.,6731.])
+    rest_wavelength = np.array([4861.,4959.,5007.,6548.,6563.,6584.,6717.,6731.])
     obs_wavelength = (1 + redshift) * rest_wavelength
     for i in range(len(line_flux)):
         if obs_wavelength[i]>9200. or obs_wavelength[i]<3800.:
@@ -103,8 +104,7 @@ if True:
         else:
             density_estimator = inference.train(max_num_epochs=epoch, resume_training=True)  # Pick `max_num_epochs` such that it does not exceed the runtime.
         posterior = inference.build_posterior(density_estimator)
-        torch.save(posterior.net, './sbi_inference_SDSS_train_1M_OII_OII_Hb_OIII_OIII_NII_Ha_NII_SII_SII_epoch_{}'.format(epoch))
+        torch.save(posterior.net, './sbi_inference_SDSS_train_1M_Hb_OIII_OIII_NII_Ha_NII_SII_SII_epoch_{}'.format(epoch))
         print('SAVED EPOCH: {}'.format(epoch))
         if inference._converged(epoch, 20):
             break
-
