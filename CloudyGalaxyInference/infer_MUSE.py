2e3aae8c2601b9ea0fa3bab1489d311a1df7100a
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-11-14 12:36:25 +0000

    Update readability MUSE SBI inference code.

--- CloudyGalaxyInference/infer_MUSE.py
+++ CloudyGalaxyInference/infer_MUSE.py
@@ -15,6 +15,15 @@ from galactic_extinction_correction import galactic_extinction_correction, rest_
 
 large_number = 1e10
 
+# Inputs
+redshift   = 0.005224
+ra         = 185.4788625     # in degrees
+dec        = 4.4737744       # in degrees
+MUSE_cube  = fits.open("/Users/dirk/Documents/PhD/scripts/catalogs/MUSE/ADP.2021-07-16T10_20_56.494.fits")
+model_path = '/Users/dirk/Documents/PhD/scripts/CloudyGalaxy/models/test_model_high_res/'
+posterior_network = 'sbi_inference_MUSE_train_1M_Hb_OIII_OIII_NII_Ha_NII_epoch_35'
+output_file = 'sbi_fits_MUSE_train_1M_Hb_OIII_OIII_NII_Ha_NII_epoch_35.npy'
+
 # import photoionization models
 path = '/Users/dirk/Documents/PhD/scripts/CloudyGalaxy/models/test_model_high_res/'
 model_labels = list(np.load(path + 'test_model_high_res_age_2Myr_unattenuated_emission_line_labels.npy'))
@@ -33,13 +42,9 @@ line_flux_labels = [label+'_FLUX' for label in line_labels]
 line_flux_err_labels = [label+'_FLUX_ERR' for label in line_labels]
 line_wavelengths = [4862., 4960., 5008., 6549., 6564., 6585.]
 
-foreground_correction_factors = galactic_extinction_correction(185.4788625*u.degree, 4.4737744*u.degree, line_wavelengths*u.angstrom, np.ones_like(line_wavelengths)*(u.erg * u.cm**-2 * u.s**-1)).value
+foreground_correction_factors = galactic_extinction_correction(ra*u.degree, dec*u.degree, line_wavelengths*u.angstrom, np.ones_like(line_wavelengths)*(u.erg * u.cm**-2 * u.s**-1)).value
 
-MUSE_cube = fits.open("/Users/dirk/Documents/PhD/scripts/catalogs/MUSE/ADP.2021-07-16T10_20_56.494.fits")
-
-redshift  = 0.005224
 cube_dims = MUSE_cube[6].data.shape
-
 MUSE_df = pd.DataFrame()
 
 MUSE_df['SPAXELID'] = np.arange(len(MUSE_cube[6].data.reshape(-1)))
@@ -64,7 +69,7 @@ MUSE_df['H_ALPHA_FLUX_ERR'] = MUSE_cube[31].data.reshape(-1) * foreground_correc
 MUSE_df['NII_6584_FLUX'] = MUSE_cube[36].data.reshape(-1) * foreground_correction_factors[5]
 MUSE_df['NII_6584_FLUX_ERR'] = MUSE_cube[37].data.reshape(-1) * foreground_correction_factors[5]
 
-MUSE_df=MUSE_df.fillna(0.0)
+MUSE_df = MUSE_df.fillna(0.0)
 
 # Fill analysis dataframe
 data_df = MUSE_df
@@ -89,9 +94,13 @@ def calc_log_gas(logZ, xi, logtau):
     Zsun = 0.0142  # Asplund (2009) photospheric mass fraction
     return np.log10(0.2 * 10 ** logtau / (xi * 10 ** logZ * Zsun))
 
-
-# Fill undetected emission lines with noise
 def prepare_input(flux, flux_error):
+    '''
+    fill unrealistic values with 0.0
+    :param flux:
+    :param flux_error:
+    :return:
+    '''
     for i in range(len(flux)):
         if flux_error[i] <= 0. or np.isinf(flux_error)[i]:
             flux_error[i] = 0.0
@@ -99,10 +108,17 @@ def prepare_input(flux, flux_error):
     output = np.expand_dims(np.concatenate([flux, flux_error]), axis=0)
     return torch.from_numpy(output)
 
-
 # Model fitting function
 def fit_model_to_df(index, prior_min=np.array([[0., -1., -4., 0.1, -2.]]),
                     prior_max=np.array([[5., 0.7, -1., 0.6, 0.6]]), plotting=False):
+    '''
+    Function to transform fitting output to parameter percentile values in a numpy array.
+    :param index:
+    :param prior_min:
+    :param prior_max:
+    :param plotting:
+    :return:
+    '''
     parameters_out = np.ones((25)) * -999.
     galaxy = data_df[data_df['SPAXELID'] == index]
     parameters_out[0] = galaxy['SPAXELID'].to_numpy()[0]
@@ -134,25 +150,24 @@ def fit_model_to_df(index, prior_min=np.array([[0., -1., -4., 0.1, -2.]]),
 
 # Define number of dimensions and prior
 num_dim = 5
-
 prior = utils.BoxUniform(low=torch.tensor([-large_number, -large_number, -large_number, -large_number, -large_number]),
                          high=torch.tensor([large_number, large_number, large_number, large_number, large_number]))
 
 
 # Create a fake simulation to instantiate a posterior
 def fake_simulation(theta):
-    return torch.tensor([[1., 1., 1., 1., 1., 1., 1., 1., 1., 1.]])
+    return torch.tensor([[1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.]])
 
 
 # Create posterior, do minimal simulations
 posterior = infer(fake_simulation, prior, 'SNPE', num_simulations=10, )
 # Replace posterior neural net with trained neural net from file
-posterior.net = torch.load('sbi_inference_MUSE_train_1M_Hb_OIII_OIII_NII_Ha_NII_epoch_35')
+posterior.net = torch.load(posterior_network)
 parameters = np.ones((len(data_df), 25)) * -999.
 parameters[:,0] = data_df['SPAXELID'].to_numpy()
 for j in range(len(data_df)):
     parameters[j] = fit_model_to_df(data_df['SPAXELID'].iloc[j], plotting=False)
     if j % 100 == 0.0:
-        np.save('sbi_fits_MUSE_train_1M_Hb_OIII_OIII_NII_Ha_NII_epoch_35.npy', parameters)
+        np.save(output_file, parameters)
 
-np.save('sbi_fits_MUSE_train_1M_Hb_OIII_OIII_NII_Ha_NII_epoch_35.npy', parameters)
+np.save(output_file, parameters)
