6890ba0ba2c85c828105583f9179ffa1e0a04507
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-11-17 15:02:18 +0000

    Created simulate, train and infer scripts to house the code of different sections.

---
+++ CloudyGalaxyInference/infer.py
@@ -0,0 +1,118 @@
+import numpy as np
+import corner
+import matplotlib.pyplot as plt
+import torch
+
+from sbi import utils as utils
+from sbi.inference.base import infer
+large_number = 1e10
+
+def calc_log_dust(logtau):
+    '''
+    Calculate the dust surface density as in Brinchmann et al. 2013
+    :param logtau: Log optical depth at 5500 Angstrom
+    :return: Log of dust surface density
+    '''
+    return np.log10(0.2 * 10 ** logtau)
+
+
+def calc_log_gas(logZ, xi, logtau):
+    '''
+    Calculate the gas surface density as in Brinchmann et al. 2013
+    :param logZ: Log metallicity in units of solar metallicity
+    :param xi: Dust-to-metal ratio
+    :param logtau: Log optical depth at 5500 Angstrom
+    :return: Log of gas surface density
+    '''
+    Zsun = 0.0142  # Asplund (2009) photospheric mass fraction
+    return np.log10(0.2 * 10 ** logtau / (xi * 10 ** logZ * Zsun))
+
+def prepare_input(flux, flux_error):
+    '''
+    fill unrealistic values with 0.0
+    :param flux: Numpy array of emission line fluxes
+    :param flux_error: Numpy array of emission line flux errors
+    :return:
+    '''
+    for i in range(len(flux)):
+        if flux_error[i] <= 0. or np.isinf(flux_error)[i]:
+            flux_error[i] = 0.0
+            flux[i] = 0.0
+    output = np.expand_dims(np.concatenate([flux, flux_error]), axis=0)
+    return torch.from_numpy(output)
+
+def fit_model_to_data(sbi_posterior, data_flux, data_flux_error, interpolated_logOH, num_samples=10000, prior_lower_boundary=[0, -1., -4., 0.1, -2.], prior_upper_boundary=[6, 0.7, -1., 0.6, 0.6], plotting=False, plot_name='test'):
+    '''
+    Inference procedure to derive the 16, 50, 84 percentile intervals of the sbi_posterior parameters.
+    :param sbi_posterior: Trained SBI posterior
+    :param data_flux: Numpy array of emission line fluxes
+    :param data_flux_error: Numpy array of emission line flux errors
+    :param interpolated_logOH: The interpolated log(OH) values of the photoionization models
+    :param num_samples: Number of samples to draw for inference.
+    :param prior_lower_boundary: The lower boundary below which the sampled parameter values are masked
+    :param prior_upper_boundary: The lower boundary below which the sampled parameter values are masked
+    :param plotting: Option to output corner plot
+    :param plot_name: Option to set name of corner plot
+    :return:
+    '''
+    posterior_samples = sbi_posterior.sample((num_samples,), x=prepare_input(data_flux, data_flux_error))
+    posterior_samples = posterior_samples.numpy()
+
+    sample_mask = np.prod((posterior_samples > prior_lower_boundary)[:, 1:] & (posterior_samples < prior_upper_boundary)[:, 1:], axis=1) == 1
+    masked_posterior_samples = posterior_samples[sample_mask]
+
+    parameters_out = np.ones((24)) * -999.
+    if np.sum(sample_mask) / len(sample_mask) > 0.5:
+        samples_logOH = interpolated_logOH(masked_posterior_samples[:, 1:-1])
+        samples_dust = calc_log_dust(masked_posterior_samples[:, 4])
+        samples_gas = calc_log_gas(masked_posterior_samples[:, 1], masked_posterior_samples[:, 3],
+                                   masked_posterior_samples[:, 4])
+        masked_posterior_samples = np.hstack(
+            [masked_posterior_samples, np.expand_dims(samples_logOH, axis=-1), np.expand_dims(samples_dust, axis=-1),
+             np.expand_dims(samples_gas, axis=-1)])
+        parameters_out = np.percentile(masked_posterior_samples, [16, 50, 84], axis=0).T.flatten()
+        if plotting:
+            corner.corner(masked_posterior_samples, show_titles=True, quantiles=[0.16, 0.5, 0.84],
+                          labels=['Amplitude', 'Z', 'U', '$\\xi$', '$\\tau$', 'log(O/H)', 'log(dust)', 'log(gas)'])
+            plt.savefig('./corner_{}.pdf'.format(plot_name))
+            plt.close()
+    return parameters_out
+
+
+def fit_model_to_dataframe(posterior_network, dataframe, identifier_column, line_flux_labels, line_flux_error_labels, output_file, interpolated_logOH, num_samples=10000, prior_lower_boundary=[0, -1., -4., 0.1, -2.], prior_upper_boundary=[6, 0.7, -1., 0.6, 0.6], plotting=False, plot_name='test'):
+    '''
+    Inference procedure to derive the 16, 50, 84 percentile intervals of the sbi_posterior parameters for an entire dataframe.
+    :param posterior_network: SBI posterior neural network (torch)
+    :param dataframe: pandas dataframe with the data.
+    :param identifier_column: column with unique identifier of a row
+    :param line_flux_labels: labels of columns containing line fluxes
+    :param line_flux_error_labels: labels of columns containing line flux errors
+    :param output_file: Name of output file
+    :param interpolated_logOH: The interpolated log(OH) values of the photoionization models
+    :param num_samples: Number of samples to draw for inference.
+    :param prior_lower_boundary: The lower boundary below which the sampled parameter values are masked
+    :param prior_upper_boundary: The lower boundary below which the sampled parameter values are masked
+    :param plotting: Option to output corner plot
+    :param plot_name: Option to set name of corner plot
+    :return:
+    '''
+    prior = utils.BoxUniform(low=torch.tensor([-large_number, -large_number, -large_number, -large_number, -large_number]),
+                         high=torch.tensor([large_number, large_number, large_number, large_number, large_number]))
+
+    # Create a fake simulation to instantiate a posterior
+    def fake_simulation(theta):
+        return torch.tensor([[1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.]])
+
+    # Create posterior, do minimal simulations
+    posterior = infer(fake_simulation, prior, 'SNPE', num_simulations=10, )
+    # Replace posterior neural net with trained neural net from file
+    posterior.net = torch.load(posterior_network)
+    parameters = np.ones((len(dataframe), 25)) * -999.
+    parameters[:,0] = dataframe[identifier_column].to_numpy()
+    for i in range(len(dataframe)):
+        parameters[i,1:] = fit_model_to_data(posterior, dataframe[line_flux_labels].to_numpy()[i], dataframe[line_flux_error_labels].to_numpy()[i], interpolated_logOH, num_samples=num_samples, prior_lower_boundary=prior_lower_boundary, prior_upper_boundary=prior_upper_boundary, plotting=plotting, plot_name=plot_name+'_'+str(i))
+        if i % 100 == 0.0:
+            np.save(output_file+'.npy', parameters)
+
+    np.save(output_file+'.npy', parameters)
+    return
---
+++ CloudyGalaxyInference/simulate.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+
+def transmission_function(lambda_, logtau, n=-1.3):
+    '''
+    Function to calculate the transmission function. As in Charlot and Fall (2000)
+    :param lambda_: Wavelength values of the spectrum bins in Angstrom
+    :param logtau: Log optical depth at 5500 Angstrom
+    :param n: Exponent of power law. Default is -1.3 as is appropriate for birth clouds (-0.7 for general ISM).
+    :return: Transmission function for each bin in the spectrum
+    '''
+    lambda_ = np.array(lambda_)
+    return np.exp(-10**logtau * (lambda_/5500)**n)
+
+def simulation_MUSE(theta, line_wavelengths, interpolated_flux, redshift, gaussian_noise_model):
+    '''
+    Function to simulate emission line observations from photoionization models and a Gaussian noise model.
+    :param theta: Input vector containing the free parameters of the model (Amplitude, Z, U, xi, tau)
+    :param line_wavelengths: Numpy array with the rest wavelengths of the emission lines
+    :param interpolated_flux: Interpolated emission line flux from photoionization models
+    :param redshift: The redshift at which the observations are simulated.
+    :param gaussian_noise_model: Gaussian noise model class
+    :return:
+    '''
+    theta = theta.numpy()[0]
+    transmission = transmission_function(line_wavelengths, theta[-1])
+    model_line_flux = np.zeros((len(interpolated_flux)))
+    for i in range(len(interpolated_flux)):
+        model_line_flux[i] = 10**theta[0] * interpolated_flux[i](theta[1:-1])*transmission[i]
+    if redshift=='random':
+        redshift = np.random.uniform(low=0.0, high=0.5)
+    line_flux, line_flux_error = gaussian_noise_model.add_gaussian_noise(model_line_flux, (1 + redshift) * line_wavelengths, np.ones_like(line_wavelengths) * np.random.uniform(low=0.0, high=100.0))
+    tensor_out = np.expand_dims(np.hstack([line_flux, line_flux_error]),axis=0)
+    tensor_out = torch.from_numpy(tensor_out).to(torch.float32)
+    return tensor_out
