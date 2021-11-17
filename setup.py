9abfc8a3f0ab35582428484f955a2cf37a9b1741
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-11-17 10:39:15 +0000

    Create setup.py file for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="CloudyGalaxyInference",
+    version="0.1",
+    author="Dirk Scholte",
+    author_email="dirk.scholte.20@ucl.ac.uk",
+    description="Python package to fit photoionization models to emission line measurements using SBI.",
+    packages=["CloudyGalaxyInference"],
+    install_requires=["numpy", "scipy", "astropy", "matplotlib", "corner", "sbi", "torch", "dustextinction", "dustmaps"]
+)
