8fbab2c8d37fd1c8a2ba7aee566ed5cb333ea3af
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-12-03 14:02:15 +0000

    Add random redshift selection from array.

--- CloudyGalaxyInference/simulate.py
+++ CloudyGalaxyInference/simulate.py
@@ -12,7 +12,7 @@ def transmission_function(lambda_, tau, n=-1.3):
     lambda_ = np.array(lambda_)
     return np.exp(-tau * (lambda_/5500)**n)
 
-def simulation_MUSE(theta, line_wavelengths, interpolated_flux, redshift, gaussian_noise_model):
+def simulation_MUSE(theta, line_wavelengths, interpolated_flux, redshifts, gaussian_noise_model):
     '''
     Function to simulate emission line observations from photoionization models and a Gaussian noise model.
     :param theta: Input vector containing the free parameters of the model (Amplitude, Z, U, xi, tau)
@@ -27,8 +27,10 @@ def simulation_MUSE(theta, line_wavelengths, interpolated_flux, redshift, gaussi
     model_line_flux = np.zeros((len(interpolated_flux)))
     for i in range(len(interpolated_flux)):
         model_line_flux[i] = 10**theta[0] * interpolated_flux[i](theta[1:-1]) * transmission[i]
-    if redshift=='random':
+    if redshifts=='random':
         redshift = np.random.uniform(low=0.0, high=0.5)
+    else:
+        redshift = np.random.choice(redshifts)
     line_flux, line_flux_error = gaussian_noise_model.add_gaussian_noise(model_line_flux, (1 + redshift) * line_wavelengths, np.ones_like(line_wavelengths) * np.random.uniform(low=0.0, high=100.0))
     tensor_out = np.expand_dims(np.hstack([line_flux, line_flux_error]),axis=0)
     tensor_out = torch.from_numpy(tensor_out).to(torch.float32)
